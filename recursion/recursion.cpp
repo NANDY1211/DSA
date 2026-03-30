e112a2bc9e46c0e3995894623483de54890e14cc
NANDY1211 <nandhakishore1211@gmail.com>
2026-03-31 00:43:09 +0530

    intermediate problem done using recursion

--- recursion/recursion.cpp
+++ recursion/recursion.cpp
@@ -51,6 +51,44 @@ void print_desc_back_track(unsigned int times,unsigned int init = 1)
     std::cout<<"Linearly desc value :: "<<init<<std::endl;
 }
 
+//Sum of n numbers using parameterized way
+void sum_n_param(unsigned int limit,unsigned int sum = 0)
+{
+    if(limit<1){
+        std::cout<<"Sum of n is :: "<<sum<<std::endl;
+        return;
+    }
+    sum_n_param(limit-1,sum+limit);
+}
+
+
+//Sum of n numbers using functional way
+unsigned int sum_n_func(unsigned int limit)
+{
+    if(limit == 1){
+        return 1;
+    }
+    return limit + sum_n_func(limit-1);
+}
+
+//factorial numbers using recursion and parameterized way
+void fact_param(unsigned int limit,unsigned fact = 1)
+{
+    if(limit <1){
+        std::cout<<"Factorial using param way is :: "<<fact<<std::endl;
+        return;
+    }
+    fact_param(limit-1,fact*limit);
+}
+
+//factorial numbers using recursion and functional way
+unsigned int fact_func(unsigned int limit)
+{
+    if(limit == 1){
+        return 1;
+    }
+    return limit * fact_func(limit-1);
+}
 
 int main()
 {
@@ -63,5 +101,14 @@ int main()
     print_asc_back_track(5);
     NEW_LINE;
     print_desc_back_track(5);
+    NEW_LINE;
+    sum_n_param(3);
+    NEW_LINE;
+    std::cout << "Sum using functional :: "<<sum_n_func(6)<<std::endl;
+    NEW_LINE;
+    fact_param(5);
+    NEW_LINE;
+    std::cout<<"Factorial using functional way is :: "<<fact_func(10)<<std::endl;
+    
     return 0;
 }
