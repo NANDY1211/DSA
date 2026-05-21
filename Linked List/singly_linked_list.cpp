11695aedfe8e9a8677a6db3cb9580e7635560791
NANDY1211 <nandhakishore1211@gmail.com>
2026-05-22 00:57:49 +0530

    Singly Linked list Implemented

--- Linked List/singly_linked_list.cpp
+++ Linked List/singly_linked_list.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
 
-
-//Defining a node structure
-
+//Defining a Singly linked list
 struct Node
 {
-    //Default Access specifier is public for struct
     int data;
-    Node* next;
+    Node* next; //Pointer to the next Node
 
-    //constructor
-    Node(int value) : data(value) , next(nullptr){}
+    Node(int value) : data(value) , next(nullptr) {}
 };
 
-//Define Linked List class
 class LinkedList
 {
     private:
-    //head and tail to easily access linked list
-        Node* head;
-        Node* tail;
+    Node* head; //Head pointer to track the start of the node
+    Node* tail; // tail pointer to track the end of the node
+
     public:
-        //Default constructor
-        LinkedList()
+        LinkedList() : head(nullptr) , tail(nullptr) {}
+
+        //CreateNode
+        Node* createNode(int value)
         {
-            head = nullptr;
-            tail = nullptr;
+            return new Node(value);
         }
 
-        //Clearing linked list
+        //clearing the list
         void clear()
         {
-            while(head != nullptr)
+            if(head == nullptr)
             {
-                delete_head();
+                std::cout<<"Nothing to clear\n";
+                return;
+            }
+            if(head == tail)
+            {
+                delete head;
+                head = tail = nullptr;
+                return;
+            }
+
+            Node* traverse = head;
+
+            while(traverse != nullptr)
+            {
+                Node* delNode = traverse;
+                traverse = traverse->next;
+                delete delNode;
             }
         }
 
@@ -43,302 +55,241 @@ class LinkedList
             clear();
         }
 
-        //Creating a Node
-        Node* createNode(int val)
-        {
-            return new Node(val);
-        }
+        /* Insertion operation */
 
+        //Inserting node at front
 
-        /* Insert operations */
-        
-        // Head insert - O(1)
-        void insert_head(int val)
+        void pushFront(int value)
         {
-            //If initial insertion
             if(head == nullptr)
             {
-                Node* newNode = createNode(val);
-                head = newNode;
-                tail = newNode;
-                return;
-            }
-
-            //If already node are there
-            Node* newNode = createNode(val); //create a node
-            newNode->next = head; // link new node next to head node 
-            head = newNode; // move the head to new node
-        }
-
-        //tail insert - O(1)
-        void insert_tail(int val)
-        {
-            //if no node are present already
-            if(head == nullptr)
-            {
-                Node* newNode = createNode(val);
-                head = newNode;
-                tail = newNode;
-                return;
-            }
-
-            //If node already present
-            Node* newNode = createNode(val); //create a node
-            tail->next = newNode; // link new node after tail node
-            tail = newNode; //move the tail pointer to new node
-        }
-
-        //Insert at position - O(n)
-        void insertAt(int pos,int val)
-        {
-            //if no node is present or position is initial
-            if(head == nullptr || pos < 1)
-            {
-                Node* newNode = createNode(val);
+                Node* newNode = createNode(value);
                 head = tail = newNode;
                 return;
             }
 
-            Node* temp = head; // create a temp pointer to traverse
-            Node* newNode = createNode(val); // create a new node
-            //Traverse through the position
-            for(int i=0;i<pos-1;i++)
-            {
-                //while traversing if position exceeds the Node present
-                if(temp == nullptr || temp-> next == nullptr)
-                {
-                    insert_tail(val);
-                    return;
-                }
-                //If not exceeds the nodes present
-                temp = temp->next; //this is the node before position
-            }
-            newNode->next = temp->next; //new node points to the node after position
-            temp->next = newNode; // previous node to position should points to the new node
+            Node* newNode = createNode(value);
+            newNode->next = head;
+            head = newNode;
         }
 
+        //Inserting node at back
 
-        /* Delete Operation*/
-
-        //Delete at head - O(1)
-        void delete_head()
+        void pushBack(int value)
         {
-            //If no Nodes are there
             if(head == nullptr)
             {
-                std::cout<<"No Nodes to delete\n";
+                Node* newNode = createNode(value);
+                head = tail = newNode;
                 return;
             }
 
-            //If nodes are there
-            Node* temp = head; // store head in temp
-            head = head->next; // move the head pointer to next Node
-            delete temp; // delete the previous head node
+            Node* newNode = createNode(value);
+            tail->next = newNode;
+            newNode->next = nullptr;
+            tail = newNode;
         }
 
-        //Delete at tail - O(n)
-        void delete_tail()
-        {
-            //If no Nodes are there
-            if(head == nullptr)
-            {
-                std::cout<<"No Nodes to delete\n";
+        //Inserting Node at specific position
+        void pushAt(int pos, int value) {
+            if (pos <= 1) { // insert at head
+                pushFront(value);
                 return;
             }
 
-            //If nodes present
-            Node* temp = head; //create a temp node to traverse
-
-            while(temp->next != tail)
-            {
-                temp = temp->next; // traverse through the node before tail node
+            Node* prev = head;
+            for (int i = 1; i < pos - 1 && prev != nullptr; i++) {
+                prev = prev->next;
             }
 
-            delete tail; // delete the tail node
-            tail = temp; // make before node to tail as tail 
-            temp->next = nullptr; // make next to tail as nullptr
-        }
-
-        //Delete node in specific position
-        void delete_value(int val)
-        {
-            //If no Nodes are there
-            if(head == nullptr)
-            {
-                std::cout<<"No Nodes to delete\n";
+            if (prev == nullptr) {
+                std::cout << "Position exceeds list length, inserting at back\n";
+                pushBack(value);
                 return;
             }
 
-            Node* prev = nullptr;
-            Node* current = head;
+            Node* newNode = createNode(value);
+            newNode->next = prev->next;
+            prev->next = newNode;
 
-            while(current != nullptr && (current->data != val))
-            {
-                prev = current; // points to the previous node
-                current = current->next; //points to the current node
+            if (newNode->next == nullptr) { // update tail if inserted at end
+                tail = newNode;
             }
+        }
 
-            //If value not found
-            if(current == nullptr)
+        /* Removal operation */
+
+        //Remove at front
+
+        void popFront()
+        {
+            if(head == nullptr)
             {
-                std::cout<<"Value not found\n";
+                std::cout<<"List is Empty\n";
                 return;
             }
 
-            //Bypass and delete
-            prev->next = current-> next;
-            if(current == tail)
+            if(head == tail)
             {
-                tail = prev;
+                delete head;
+                head = tail = nullptr;
+                return;
             }
-            delete current;
-            
+
+            Node* delNode = head;
+            head = head->next;
+            delete delNode;
         }
 
-        /* Searching an Value - O(n) */
+        //Remove at back
 
-        int search(int val) const
+        void popBack()
         {
-            // if no node is present
             if(head == nullptr)
             {
-                std::cout<<"No Node to search \n";
-                return -1;
+                std::cout<<"List is empty\n";
+                return;
             }
 
-            //if node present
-            Node* traverse = head; // create a pointer to traverse
+            if(head == tail)
+            {
+                delete head;
+                head = tail = nullptr;
+                return;
+            }
+
+            Node* delNode = tail;
+            Node* prev = head;
+            while(prev->next!=tail)
+            {
+                prev = prev->next;
+            }
+            prev->next = nullptr;
+            tail = prev;
+            delete delNode;
+        }
+
+        //Remove at specific position
+
+        void popAt(int pos)
+        {
+            if(head == nullptr)
+            {
+                std::cout<<"List is empty\n";
+                return;
+            }
+            if(head == tail)
+            {
+                delete head;
+                head = tail = nullptr;
+                return;
+            }
+            if(pos<1)
+            {
+                popFront();
+                return;
+            }
+
+
+            Node* prev = head;
+
+            for(int i=0;i<pos-1 && prev!=nullptr;i++)
+            {
+                prev = prev->next;
+            }
+            if(prev == nullptr)
+            {
+                popBack();
+                return;
+            }
+            Node* after = prev->next;
+            prev->next = after->next;
+            delete after;
+        }
+
+        //Search in the list
+        void search(int value)
+        {
+            if(head == nullptr)
+            {
+                std::cout<<"List is empty , nothing to search\n";
+                return;
+            }
+
+            Node* traverse = head;
             int index = 0;
-            while(traverse != nullptr) // traverse and match the value
+            while(traverse != nullptr && traverse->data != value)
             {
-                if(traverse->data == val)
-                {
-                    return index;
-                }
-
                 traverse = traverse->next;
                 index++;
             }
-            //if value not found
             if(traverse == nullptr)
             {
-                std::cout<<"Value is not present in the list\n";
-                return -1;
-            }
-
-            return -1;
-
-        }
-
-        /* Reverse a linked list - O(n) */
-
-        void reverse()
-        {
-            //If no node present
-            if(head == nullptr)
-            {
-                std::cout<<"No node is present\n";
+                std::cout<<"There is no such value present in list\n";
                 return;
             }
 
-            //taking three pointer to track previous , current and next node
-            Node* prev = nullptr;
-            Node* current = head;
-            Node* next = nullptr;
+            std::cout<<"Value "<<value<<" present in the index "<<index<<std::endl;
+        }
 
-            //Traversing through the linked list
-            while(current != nullptr)
+        //Reverse 
+        void reverse()
+        {
+            if(head == nullptr)
             {
-                next = current->next;
+                std::cout<<"List is empty , nothing to be reversed\n";
+                return;
+            }
+
+            Node* prev = nullptr;
+            Node* after = nullptr;
+            Node* current = head;
+            tail = head;
+            while(current!=nullptr)
+            {
+                after = current->next;
                 current->next = prev;
                 prev = current;
-                current = next;
+                current = after;
             }
 
             head = prev;
-
         }
 
-        /* Find and replace an element - O(n)*/
-
-        void findAndReplace(int findElement,int replaceElement)
+        //Display list
+        void show()
         {
-
-            //If no node present
             if(head == nullptr)
             {
-                std::cout<<"No node is present\n";
+                std::cout<<"List is empty\n";
                 return;
             }
 
-            Node* traverse = head; // creating a pointer to traverse through linked list
 
-            while(traverse != nullptr)
-            {
-                if(traverse->data == findElement) // if the find element matched
-                {
-                    traverse->data = replaceElement;
-                    return;
-                }
-                traverse = traverse->next;
-            }
-            //If find element not found
-            if(traverse == nullptr)
-            {
-                std::cout<<"Can't find the element . So can't be replaced\n";
-            }
-
-        }
-
-
-        //Display nodes present
-        void display()
-        {
             Node* traverse = head;
-
-            if(head == nullptr)
-            {
-                std::cout<<"There is no element to traverse\n";
-                return;
-            }
-
             while(traverse != nullptr)
             {
                 std::cout<<traverse->data<<"->";
                 traverse = traverse->next;
             }
-            std::cout<<"NULL";
+            std::cout<<"NULL\n";
         }
 
 };
 int main()
 {
-
-    LinkedList llObj;
-    llObj.insert_head(1);
-    llObj.insert_head(11);
-    llObj.insert_tail(2);
-    llObj.insert_tail(3);
-    llObj.insert_tail(4);
-    llObj.insertAt(2,22);
-    llObj.insertAt(4,33);
-    llObj.insertAt(6,44);
-    llObj.delete_head();
-    llObj.delete_tail();
-    llObj.delete_value(33);
+    LinkedList list;
+    list.pushFront(1);
+    list.pushBack(2);
+    list.pushAt(3,3);
+    list.pushBack(4);
+    list.pushAt(5,5);
+    list.reverse();
 
 
-    std::cout<<"Searching element in linked list - Position : "<<llObj.search(99)<<std::endl;
+    
 
-    llObj.display();
 
-    std::cout <<"\nAfter reversing the linked list\n";
-    llObj.reverse();
-    llObj.display();
-
-    std::cout<<"\nAfter finding and replacing linked list element\n";
-    llObj.findAndReplace(3,33);
-    llObj.display();
+    list.show();
     return 0;
-
 }
