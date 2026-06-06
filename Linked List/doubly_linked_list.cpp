4e46cf0c28b641ff7c627207c347fc13663c3d18
NANDY1211 <nandhakishore1211@gmail.com>
2026-06-06 19:03:20 +0530

    update doubly linked list

--- Linked List/doubly_linked_list.cpp
+++ Linked List/doubly_linked_list.cpp
@@ -1,281 +1,280 @@
 #include <iostream>
 
-//Defining a doubly linked list structure
-
 struct Node
 {
     int data;
     Node* prev;
     Node* next;
 
-    Node(int value) : data(value) , prev(nullptr), next(nullptr) {}
-
+    Node(int value) : data(value) , prev(nullptr) , next(nullptr){}
 };
 
 
-//Defining Doubly Linked list functionality
-class LinkedList
+class DoublyLinkedList
 {
     private:
         Node* head;
         Node* tail;
+        int size_;
+
     public:
-        LinkedList() : head(nullptr) , tail(nullptr) {}
+        DoublyLinkedList() : head(nullptr),tail(nullptr),size_(0){}
 
-        //clearing Doubly linked list
-        void clear()
-        {
-            if(head == nullptr)
-            {
-                std::cout<<"List is empty , Nothing to be clear\n";
-                return;
-            }
-
-            if(head == tail)
-            {
-                delete head;
-                head = tail = nullptr;
-                return;
-            }
-
-            Node* traverse = head;
-            while(traverse != nullptr)
-            {
-                Node* delNode = traverse;
-                traverse = traverse->next;
-            }
-            head = tail = nullptr;
-        }
-
-        ~LinkedList()
-        {
-            clear();
-        }
-
-
-        //Create node
         Node* createNode(int value)
         {
             return new Node(value);
         }
 
-        /* Insertion operation */
+        void clear()
+        {
+            if(head==nullptr) return;
 
-        //Insert at front
+            if(head==tail)
+            {
+                delete head;
+                size_--;
+                head=tail=nullptr;
+                return;
+            }
+
+            Node* traverse=head;
+            while(traverse)
+            {
+                Node* delNode=traverse;
+                traverse=traverse->next;
+                delete delNode;
+                size_--;
+            }
+            head=tail=nullptr;
+        }
+
+        ~DoublyLinkedList(){clear();}
+
+
         void pushFront(int value)
         {
-            if(head == nullptr)
+            if(head==nullptr)
             {
-                Node* newNode = createNode(value);
-                head = tail = newNode;
+                Node* newNode=createNode(value);
+                head=tail=newNode;
+                newNode->prev=nullptr;
+                newNode->next=nullptr;
+                size_++;
                 return;
             }
 
-            Node* newNode = createNode(value);
-            newNode->next = head;
-            head->prev = newNode;
-            head = newNode;
+            Node* newNode=createNode(value);
+
+            newNode->next=head;
+            head->prev=newNode;
+            newNode->prev=nullptr;
+            head=newNode; 
+            size_++;           
         }
 
-        //Insert at back
         void pushBack(int value)
         {
-            if(head == nullptr)
+            if(head==nullptr)
             {
-                Node* newNode = createNode(value);
-                head = tail = newNode;
+                Node* newNode=createNode(value);
+                head=tail=newNode;
+                newNode->prev=nullptr;
+                newNode->next=nullptr;
+                size_++;
                 return;
             }
 
-            Node* newNode = createNode(value);
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
+            Node* newNode=createNode(value);
+            newNode->prev=tail;
+            newNode->next=nullptr;
+            tail->next=newNode;
+            tail=newNode;
+            size_++;
         }
 
-        //Insert at specific position
         void pushAt(int pos,int value)
         {
-            if(head == nullptr || pos<1)
+            if(head==nullptr)
+            {
+                Node* newNode=createNode(value);
+                head=tail=newNode;
+                size_++;
+                return;
+            }
+
+            if(pos<1)
             {
                 pushFront(value);
                 return;
             }
-
-            Node* prev = head;
-            for(int i=1;i<pos-1 && prev!=nullptr;i++)
-            {
-                prev = prev->next; 
-            }
-            if(prev->next == nullptr)
+            if(pos>=size_)
             {
                 pushBack(value);
                 return;
             }
-            Node* newNode = createNode(value);
-            Node* after = prev->next;
-            newNode->prev = prev;
-            newNode->next = after;
-            after->prev = newNode;
-            prev->next = newNode;
+
+            Node* prev=head;
+
+            for(int i=0;i<pos and prev;i++)
+            {
+                prev=prev->next;
+            }
+
+            Node* newNode=createNode(value);
+            Node* after=prev->next;
+            newNode->prev=prev;
+            newNode->next=after;
+            prev->next=newNode;
+            if(after)
+                after->prev=newNode;
+
+            size_++;
+
         }
 
-        //Remove from front
-        void removeFront()
+        void popFront()
         {
-            if(head == nullptr)
-            {
-                std::cout<<"List is empty . nothing to remove\n";
-                return;
-            }
-            if(head == tail)
+            if(head==nullptr) return;
+
+            if(head==tail)
             {
                 delete head;
-                head = tail = nullptr;
+                size_--;
+                head=tail=nullptr;
                 return;
             }
-            Node* delNode = head;
-            head = head->next;
+
+            Node* delNode=head;
+            head=head->next;
+            head->prev=nullptr;
             delete delNode;
+            size_--;
         }
 
-        //Remove from back
-        void removeBack()
+
+        void popBack()
         {
-            if(head == nullptr)
-            {
-                std::cout<<"List is empty . nothing to remove from back\n";
-                return;
-            }
-            if(head == tail)
+            if(head==nullptr) return;
+
+            if(head==tail)
             {
                 delete head;
-                head = tail = nullptr;
+                size_--;
+                head=tail=nullptr;
                 return;
             }
-            Node* delNode = tail;
-            tail = tail->prev;
-            tail->next = nullptr;
+
+            Node* delNode=tail;
+            tail=tail->prev;
+            tail->next=nullptr;
             delete delNode;
-
+            size_--;
         }
 
-        void removeAt(int pos)
+        void popAt(int pos)
         {
-            if(head == nullptr)
-            {
-                std::cout<<"List is empty . nothing to remove\n";
-                return;
-            }
-            if(head == tail)
-            {
-                delete head;
-                head = tail = nullptr;
-                return;
-            }
+            if(head==nullptr) return;
+
             if(pos<1)
             {
-                removeFront();
+                popFront();
                 return;
             }
-            Node* prev = head;
-            for(int i=0;i<pos-1 && prev!=nullptr;i++)
+
+            if(pos>=size_)
             {
-                prev = prev->next;
-            }
-            if(prev == nullptr)
-            {
-                removeBack();
+                popBack();
                 return;
             }
-            Node* delNode = prev->next;
-            prev->next = delNode->next;
-            delNode->next->prev = prev;
-            delete delNode;
+
+            Node* current=head;
+
+            for(int i=0;i<pos and current;i++)
+            {
+                current=current->next;
+            }
+            if(!current) return;
+            Node* before=current->prev;
+            Node* after=current->next;
+
+            before->next=after;
+            after->prev=before;
+            delete current;
+            size_--;
         }
 
-        void find(int value)
+        void findPosition(int value)
         {
-            if(head == nullptr)
+            if(head==nullptr) return;
+            if(head->data==value)
             {
-                std::cout<<"List is empty , nothing to search\n";
+                std::cout<<"Element found at 0\n";
                 return;
             }
 
-
-            Node* traverse = head;
-            int index = 0;
-            while(traverse!=nullptr && traverse->data != value)
+            Node* fTraverse=head;
+            Node* lTraverse=tail;
+            int fPos=0;
+            int lPos=size_-1;
+            while(fTraverse and lTraverse)
             {
-                traverse = traverse->next;
-                index++;
+                if(fTraverse->data==value)
+                {
+                    std::cout<<"Element found at "<<fPos<<std::endl;
+                    return;
+                }
+                else if(lTraverse->data==value)
+                {
+                    std::cout<<"Element found at "<<lPos<<std::endl;
+                    return;
+                }
+                fTraverse=fTraverse->next;
+                lTraverse=lTraverse->prev;
+                fPos++;
+                lPos--;
             }
-            if(traverse == nullptr)
-            {
-                std::cout<<"Value is not present in list\n";
-                return;
-            }
-            std::cout<<"Value "<<value<<" is present at "<<index<<" index\n";
 
+            std::cout<<"Element is not present in the list\n";
         }
-
-        void reverse()
-        {
-            if(head == nullptr || head == tail)
-            {
-                std::cout<<"List is empty/one element , can't reverse\n";
-                return;
-            }
-
-            Node* prev = nullptr;
-            Node* current = head;
-            Node* after = nullptr;
-            tail = head;
-
-            while(current!=nullptr)
-            {
-                after = current->next;
-                current->next = prev;
-                prev = current;
-                current = after;
-            }
-            head = prev;
-        }
-
 
         void show()
         {
-            if(head == nullptr)
-            {
-                std::cout<<"List is empty nothing to be displayed\n";
-                return;
-            }
+            if(head==nullptr) return;
 
-            Node* traverse = head;
-            while(traverse != nullptr)
+            Node* traverse=head;
+            while(traverse)
             {
                 std::cout<<traverse->data<<"<->";
-                traverse = traverse->next;
+                traverse=traverse->next;
             }
-            std::cout<<"NULL\n";
+            std::cout<<"NULL";
         }
 
-
+        void size()
+        {
+            std::cout<<"Size of the Doubly Linked List "<<size_<<std::endl;
+        }
 };
 int main()
 {
+    DoublyLinkedList list;
 
-    LinkedList list;
-    list.pushFront(1);
+    list.pushFront(0);
+    list.pushBack(1);
     list.pushBack(2);
+    list.pushBack(3);
     list.pushBack(4);
-    list.pushAt(3,3);
-    list.pushAt(5,5);
+    list.pushBack(7);
+    list.pushAt(4,5);
+    list.pushAt(5,6);
 
-    list.reverse();
-    list.reverse();
-    list.find(4);
+    list.popAt(3);
 
+    list.size();
+
+
+    list.findPosition(5);
     list.show();
+
     return 0;
 }
