bd0095ad2bb44d52b090ca507c6cbf218b360118
NANDY1211 <nandhakishore1211@gmail.com>
2026-07-05 23:55:26 +0530

    Implementation of stack using array updated

--- Stack/stack_using_array.cpp
+++ Stack/stack_using_array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 
 
 class Stack
@@ -7,82 +8,97 @@ class Stack
         int top;
         int capacity;
         int* arr;
-
+    
     public:
-        Stack(int size) : top(-1)
+        Stack(int size = 0)
         {
-            capacity=size;
-            arr = new int[size];
+            top=-1;
+            if(size>0)
+            {
+                capacity=size;
+                arr = new int[size];  
+            }
         }
 
-        ~Stack(){delete[] arr;}
+        ~Stack()
+        {
+            delete[] arr;
+        }
 
-
-        //Pushing an element
         void push(int val)
         {
-            if(top==capacity-1)
+            if(top>=capacity)
             {
-                std::cout<<"Stack Overflow!!!"<<std::endl;
+                std::cout<<"Stack Overflow\n";
                 return;
             }
             arr[++top] = val;
+            std::cout<<"Pushed "<<arr[top]<<std::endl;
         }
+
 
         int pop()
         {
-            if(top==-1) 
+            if(top==-1)
             {
                 std::cout<<"Stack underflow\n";
                 return -1;
             }
 
-
-            std::cout<<"Popped element \t"<<arr[--top]<<std::endl;
+            std::cout<<"Popped "<<arr[--top]<<std::endl;
             return arr[top];
-
         }
+
 
         int size()
         {
-            return top+1;
+            return (top+1);
         }
 
         int peek()
         {
-            if(top==-1) return -1;
-            return arr[top];
-        }
-
-        void show()
-        {
             if(top==-1)
             {
                 std::cout<<"Stack is empty\n";
-                return;
+                return -1;
             }
+
+            return arr[top];
+        }
+
+
+        bool isEmpty()
+        {
+            if(top==-1)
+            {
+                std::cout<<"Stack is Empty\n";
+                return true;
+            }
+            return false;
+        }
+
+
+        void show()
+        {
             for(int i=0;i<=top;i++)
             {
                 std::cout<<arr[i]<<",";
             }
             std::cout<<"\n";
         }
-    
 };
-
 int main()
 {
-    Stack st(5);
 
+    Stack st(10);
     st.push(1);
     st.push(2);
     st.push(3);
-    std::cout<<"Top element is "<<st.peek()<<std::endl;
     st.pop();
-    std::cout<<"Top element is "<<st.peek()<<std::endl;
+    st.pop();
 
-    std::cout<<"Size of the stack is "<<st.size()<<std::endl;
-
+    std::cout<<"Top of the stack "<<st.peek()<<std::endl;
+    st.isEmpty();
     st.show();
     return 0;
 }
